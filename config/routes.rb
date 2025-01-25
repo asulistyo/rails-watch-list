13c3c1c0c167a1592de6dd25b9dd4a463b4523e5
amandasulistyo <amandaz.sulistyo@gmail.com>
2025-01-25 18:00:52 +0800

    mostly done with list items. add bookmarks route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get 'lists', to: 'lists#index'
+  resources :lists, only: [:index, :new, :create, :show]
+    resources :bookmarks, only: [:new, :create]
 end
